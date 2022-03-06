f698e758d12017485f3de535fd30b44d24739568
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-06 13:39:23 -0700

    finish beverages controller function, and make beverages views index.ejs

--- controllers/beverages.js
+++ controllers/beverages.js
@@ -2,7 +2,16 @@ import { Checkin } from '../models/checkin.js'
 import { Beverage } from '../models/beverage.js'
 
 function index(req, res) {
-  console.log('beverage sanity');
+  Beverage.find({})
+  .then(beverages => {
+  res.render('beverages/index', {
+    title: 'Add beverages',
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/beverages")
+  })
 }
 
 export {
