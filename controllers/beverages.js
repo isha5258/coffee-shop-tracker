59eef151a9c3577728572f132603f92229eda2fb
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-06 13:04:06 -0700

    make beverages route and controller function

--- controllers/beverages.js
+++ controllers/beverages.js
@@ -1,7 +1,10 @@
 import { Checkin } from '../models/checkin.js'
 import { Beverage } from '../models/beverage.js'
 
+function index(req, res) {
+  console.log('beverage sanity');
+}
 
 export {
-  
+index,
 }
--- routes/beverages.js
+++ routes/beverages.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-
+import * as beveragesCtrl from '../controllers/beverages.js'
 
 const router = Router()
 
 // GET - localhost:3000/beverages
-
+router.get('/', beveragesCtrl.index)
 
 export {
   router
