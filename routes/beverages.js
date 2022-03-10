9ed0058a0ffcd1ab4cfffecfda7cae342377988e
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-10 13:39:55 -0700

    make beverages css file and edit beverages view

--- routes/beverages.js
+++ routes/beverages.js
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import * as beveragesCtrl from '../controllers/beverages.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // GET - localhost:3000/beverages
-router.get('/', beveragesCtrl.index)
+router.get('/', isLoggedIn, beveragesCtrl.index)
 
 // POST - localhost:3000/beverages
-router.post('/', beveragesCtrl.create)
+router.post('/', isLoggedIn, beveragesCtrl.create)
 
 export {
   router
